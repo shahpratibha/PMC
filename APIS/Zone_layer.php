eebb8e31f66a6a6b543a52f59e2c94421b2aaad2
Abdul Aleem <abdul.aleem@techstalwarts.com>
2024-02-09 21:28:20 +0530

    dashboard page updated

--- APIS/Zone_layer.php
+++ APIS/Zone_layer.php
@@ -8,28 +8,9 @@ try {
         throw new Exception("PDO connection not found. Please check your configuration.");
     }
 
-    // Initialize variables for selected zones
-    $selectedZones = isset($_GET['selectedZones']) ? $_GET['selectedZones'] : '';
-
     // Prepare the SQL query
     $sql = 'SELECT fid, ST_AsGeoJSON(geom)::json as geom, "Zone", "Zone_Name" FROM public."Zone_layer"';
 
-    // Add WHERE clause to filter by selected zones, if provided
-    if (!empty($selectedZones)) {
-        // Convert the selected zone IDs to an array
-        $zoneIDs = explode(',', $selectedZones);
-
-        // Sanitize and quote each zone ID
-        $quotedZoneIDs = array_map(function($zoneID) use ($pdo) {
-            return $pdo->quote($zoneID);
-        }, $zoneIDs);
-
-        // Implode the quoted zone IDs with commas
-        $quotedZoneIDsString = implode(',', $quotedZoneIDs);
-
-        // Add the WHERE clause to the SQL query
-        $sql .= ' WHERE "Zone" IN (' . $quotedZoneIDsString . ')';
-    }
 
     // Query to retrieve data from the table
     $stmt = $pdo->query($sql);
