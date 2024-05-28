4a84944c4e834673bce6dcd79a0b03b6a22bbfdf
shahpratibha <pratibhashah1995@gmail.com>
2024-05-28 15:56:02 +0530

    remove null value into the 3departments

--- js/building.js
+++ js/building.js
@@ -1311,7 +1311,10 @@ $.ajax({
       console.log(formDataFromStorage);
       let contentData = '<tr>';
       for (const property in formDataFromStorage) {
-        contentData += `<tr><th>${property}</th><td>${formDataFromStorage[property]}</td></tr>`;
+        // contentData += `<tr><th>${property}</th><td>${formDataFromStorage[property]}</td></tr>`;
+        if (formDataFromStorage[property] !== null) {  // Check for null value
+          contentData += `<tr><th>${property}</th><td>${formDataFromStorage[property]}</td></tr>`;
+      }
       }
       contentData += '</tr>';
       $('#workTableData').html(contentData);
@@ -1343,7 +1346,10 @@ $.ajax({
     console.log(formDataFromStorage);
     let contentData = '<tr>';
     for (const property in formDataFromStorage) {
-      contentData += `<tr><th>${property}</th><td>${formDataFromStorage[property]}</td></tr>`;
+      // contentData += `<tr><th>${property}</th><td>${formDataFromStorage[property]}</td></tr>`;
+      if (formDataFromStorage[property] !== null) {  // Check for null value
+        contentData += `<tr><th>${property}</th><td>${formDataFromStorage[property]}</td></tr>`;
+    }
     }
     contentData += '</tr>';
     $('#workTableData').html(contentData);
