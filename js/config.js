fde8e2ad86f03909bde85c517d580ecc1bd5e2a0
AishwaryaDarekar <aishwarya.darekar@geopulsea.com>
2024-05-29 13:38:21 +0530

    asad

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 //  const API_URL = "http://localhost/pmc_test/";
-const API_URL = "http://localhost/PMC/IWMS/";
+const API_URL = "http://localhost/IWMS/";
 // const API_URL = "https://iwmsgis.pmc.gov.in/gis/iwms/";
 
