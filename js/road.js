f83e1c21e283209f1669195d980cfe01f2631c6c
ankit kushwaha <ankit.kushwaha@techstalwarts.com>
2024-06-20 10:54:53 +0530

    updated code

--- js/road.js
+++ js/road.js
@@ -1600,7 +1600,13 @@ function handleMouseMove(event) {
 
 let throttle = false; // Throttling flag to control event frequency
 
+function truncateLineToLength(geojson, maxLength) {
+  // Calculate the truncated line
+  var truncatedLine = turf.lineSliceAlong(geojson, 0, maxLength, { units: 'kilometers' });
 
+  // Return coordinates only
+  return truncatedLine.geometry.coordinates;
+}
 
 
 map.on("draw:created", function (e) {
@@ -1678,64 +1684,70 @@ map.on("draw:created", function (e) {
     if (e.layerType === "polyline") {
       var length = turf.length(e.layer.toGeoJSON(), { units: "kilometers" });
       var roadLenght = lenght;
-      if (length > roadLenght) {
-        Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "Oops...",
-          text: `The Road is longer than ${roadLenght} kilometers. Please draw a shorter Road.`,
-          showConfirmButton: false,
-          showCloseButton: true,
-          customClass: {
-            popup: 'custom-modal-class',
-            icon: 'custom-icon-class',
-            title: 'custom-title-class',
-            content: 'custom-text-class',
-            closeButton: 'custom-close-button-class'
-          },
-          showClass: {
-            popup: 'swal2-show',
-            backdrop: 'swal2-backdrop-show',
-            icon: 'swal2-icon-show'
-          },
-          hideClass: {
-            popup: 'swal2-hide',
-            backdrop: 'swal2-backdrop-hide',
-            icon: 'swal2-icon-hide'
-          },
-          didOpen: () => {
-            // Apply custom styles directly to the modal elements
-            document.querySelector('.custom-modal-class').style.width = '400px'; // Set your desired width
-            document.querySelector('.custom-modal-class').style.height = '250px'; // Set your desired height
-            document.querySelector('.custom-icon-class').style.fontSize = '10px'; // Set your desired icon size
-            document.querySelector('.custom-title-class').style.fontSize = '1.5em'; // Set your desired title size
-            document.querySelector('.custom-text-class').style.fontSize = '1em'; // Set your desired text size
-            document.querySelector('.custom-close-button-class').style.backgroundColor = '#f44336'; // Red background color
-            document.querySelector('.custom-close-button-class').style.color = 'white'; // White text color
-            document.querySelector('.custom-close-button-class').style.borderRadius = '0'; // Rounded corners
-            document.querySelector('.custom-close-button-class').style.padding = '5px'; // Padding around the close button
-            document.querySelector('.custom-close-button-class').style.fontSize = '20px'; // Font size of the close button
-          }
-        });
+     
+    }
 
-        return; // Stop further processing
+
+
+
+
+
+
+
+
+    if (length > roadLenght) {
+      
+      var truncatedCoordinates = truncateLineToLength(e.layer.toGeoJSON(), roadLenght);
+		// its returning array of coordinates convert it to geojson
+		console.log('truncatedCoordinates', truncatedCoordinates);
+
+		var truncatedLineGeoJSON = {
+			type: 'Feature',
+      color: 'red',
+			geometry: {
+				type: 'LineString',
+				coordinates: truncatedCoordinates,
+			},
+		};
+
+    // add to drawn items
+
+    var geoJsonLayer = L.geoJSON(truncatedLineGeoJSON, {
+      style: function (feature) {
+          return { color: 'red' };
       }
+  }).addTo(drawnItems);
+		// Create a new Leaflet polyline and add it to the map
+		//currentPolyline = L.polyline(truncatedCoordinates, { color: 'red' }).addTo(drawnItems);
+
+		// Optionally, you can add the GeoJSON directly to the map
+		// var geoJsonLayer = L.geoJSON(truncatedLineGeoJSON).addTo(drawnItems);
+		var layer = L.geoJSON(truncatedLineGeoJSON);
+
+		var tempGeoJSON = currentPolyline.toGeoJSON();
+      
+    
+     
+    }else {
+
+      var layer = e.layer;
+
+
+      drawnItems.addLayer(layer);
+      var geoJSON = layer.toGeoJSON();
+      var tempGeoJSON = geoJSON;
+
     }
-    var layer = e.layer;
-
-
-    drawnItems.addLayer(layer);
 
 
 
-    if (e.layerType === "polyline") {
-      var bufferWidth = width;
-      createBufferAndDashedLine(layer, roadLenght, bufferWidth);
-    }
+  
+    var bufferWidth = width;
+    createBufferAndDashedLine(layer, roadLenght, bufferWidth);
     nearestPointsStorage = []; // Reset the storage for the next drawing
 
-    var geoJSON = layer.toGeoJSON();
-    var popupContent = UpdateArea(geoJSON);
+ 
+    var popupContent = UpdateArea(tempGeoJSON);
 
     var lastDrawnPolylineId = layer._leaflet_id;
     lastDrawnPolylineIdSave = layer._leaflet_id;
