1663353cc905abeb2bb96f10616ddf2a3c3f1650
purabchavan123 <purabchavan95@gmail.com>
2024-06-29 16:23:20 +0530

    tools

--- js/garden.js
+++ js/garden.js
@@ -683,7 +683,7 @@ var customDrawControls = L.control({ position: 'topleft' });
   // Define the HTML content for the control
   customDrawControls.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'draw-control');
-    div.innerHTML = '<button class="draw_feature"  style="border:2px solid darkblue;  margin-top:75%; border-radius:5px; background-color:white; padding: 5px ;" title="Draw New Feature"> <img src="png/006-drawing.png" style="width: 20px; height: 25px; padding:3px;"></button>';
+    div.innerHTML = '<button class="draw_feature"" title="Draw New Feature"> <img src="png/006-drawing.png" style="width: 20px; height: 25px; padding:3px;"></button>';
     customDrawControlsContainer = div;
     return div;
   };
