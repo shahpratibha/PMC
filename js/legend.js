65259721e7dc3f43a58e9fadfae5439a68b8adf3
purabchavan123 <purabchavan95@gmail.com>
2024-08-05 10:55:54 +0530

    add

--- js/legend.js
+++ js/legend.js
@@ -1,9 +1,7 @@
 // GeoServer URL
 var geoserverUrl = "https://iwmsgis.pmc.gov.in/geoserver";
 var workspace = "Bhavan";
-
-// Variable to keep track of legend visibility
-var legendVisible = false; // Initialize as false since legend is hidden initially
+var legendVisible = true;
 var processedLayers = [];
 
 // Add the WMS Legend control to the map
@@ -12,41 +10,25 @@ var legendControl = L.control({ position: "topright" });
 legendControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "info legend");
 
-  // Function to fetch and populate the legend
   function updateLegend() {
-    // Clear the existing legend
     div.innerHTML = '';
 
-    // Fetch capabilities to get all layers in the 'pmc' workspace
     fetch(geoserverUrl + "/ows?service=wms&version=1.3.0&request=GetCapabilities")
       .then((response) => response.text())
       .then((data) => {
-        // Parse capabilities XML response
         var parser = new DOMParser();
         var xml = parser.parseFromString(data, "text/xml");
 
-        // Extract layer names and legend URLs for layers in the 'pmc' workspace
         var layers = xml.querySelectorAll('Layer[queryable="1"]');
         layers.forEach((layer) => {
           var layerName = layer.querySelector("Name").textContent;
-          var layerWorkspace = layerName.split(":")[0]; // Extract workspace from layer name
+          var layerWorkspace = layerName.split(":")[0];
           if (layerWorkspace === workspace && !processedLayers.includes(layerName)) {
-            var legendUrl =
-              geoserverUrl +
-              "/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=" +
-              layerName;
-            var layerParts = layerName.split(":"); // Split layer name by ":"
-            var layerDisplayName = layerParts[layerParts.length - 1]; // Take the last part as the display name
-            div.innerHTML +=
-              "<p><strong>" +
-              layerDisplayName + // Use layerDisplayName instead of layerName
-              "</strong></p>" +
-              '<img src="' +
-              legendUrl +
-              '" alt="' +
-              layerDisplayName + // Use layerDisplayName instead of layerName
-              ' legend"><br>';
-            processedLayers.push(layerName); // Add processed layer to the list
+            var legendUrl = `${geoserverUrl}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=${layerName}`;
+            var layerParts = layerName.split(":");
+            var layerDisplayName = layerParts[layerParts.length - 1];
+            div.innerHTML += `<p><strong>${layerDisplayName}</strong></p><img src="${legendUrl}" alt="${layerDisplayName} legend"><br>`;
+            processedLayers.push(layerName);
           }
         });
       })
@@ -55,14 +37,12 @@ legendControl.onAdd = function (map) {
       });
   }
 
-  // Initially update the legend
   updateLegend();
 
-  // Apply CSS to fit to bottom right, occupy 60% of screen height, and provide scrollbar
   div.style.position = "fixed";
   div.style.bottom = "0";
   div.style.right = "0";
-  div.style.height = "0"; // Start with height 0 to keep it hidden initially
+  div.style.height = "40vh";
   div.style.width = "300px";
   div.style.overflowY = "auto";
   div.style.scrollbarWidth = "thin";
@@ -70,8 +50,19 @@ legendControl.onAdd = function (map) {
   div.style.border = "2px solid darkblue";
   div.style.borderRadius = "10px";
   div.style.padding = "10px";
-  div.style.transition = "all 0.3s ease-in-out"; // Add transition for smooth animation
+  div.style.transition = "all 0.3s ease-in-out";
 
+  function toggleLegend() {
+    if (legendVisible) {
+      div.style.height = "0";
+      legendVisible = false;
+    } else {
+      div.style.height = "40vh";
+      legendVisible = true;
+    }
+  }
+
+  div.addEventListener('click', toggleLegend);
   return div;
 };
 
@@ -82,9 +73,8 @@ var collapseButton = L.control({ position: "topright" });
 
 collapseButton.onAdd = function (map) {
   var button = L.DomUtil.create("button", "collapse-button");
-  button.innerHTML = "<i class='fa-solid fa-list' style='color:darkblue;'></i>"; // Initial text
+  button.innerHTML = "<i class='fa-solid fa-list' style='color:darkblue;'></i>";
 
-  // Apply styling
   button.style.backgroundColor = "white";
   button.style.border = "2px solid darkblue";
   button.style.width = "35px";
@@ -97,17 +87,27 @@ collapseButton.onAdd = function (map) {
   button.style.display = "block";
   button.style.margin = "10px";
   button.style.cursor = "pointer";
-  button.style.transition = "background-color 0.3s ease-in-out"; // Add transition for smooth animation
+  button.style.transition = "background-color 0.3s ease-in-out";
 
-  // Toggle legend visibility when the button is clicked
-  button.onclick = function (e) {
-    e.stopPropagation(); // Stop event from propagating to document click
+  button.onclick = function () {
     var legendDiv = document.querySelector(".info.legend");
-    if (!legendVisible) {
-      legendDiv.style.height = "40vh"; // Maximize the legend
+    if (legendDiv.style.height === "0px" || legendDiv.style.display === "none") {
+      legendDiv.style.display = "block";
+      legendDiv.style.height = "40vh";
+      legendDiv.style.width = "200px";
+      legendDiv.style.top = "12%";
+      legendDiv.style.right = "3%";
+      legendDiv.style.scrollbarWidth = "thin";
+      legendDiv.style.scrollbarColor = "#163140 white";
+      legendDiv.style.borderRadius = "10px";
+      legendDiv.style.boxShadow = "5px 5px 5px rgba(0, 0, 0, 0.7)";
+      button.innerHTML = "<i class='fa-solid fa-list' style='color:darkblue;'></i>";
+      button.style.backgroundColor = "white";
       legendVisible = true;
     } else {
-      legendDiv.style.height = "0"; // Minimize the legend
+      legendDiv.style.display = "none";
+      button.innerHTML = "<i class='fa-solid fa-list' style='color:darkblue;'></i>";
+      button.style.backgroundColor = "white";
       legendVisible = false;
     }
   };
@@ -116,36 +116,27 @@ collapseButton.onAdd = function (map) {
 };
 
 collapseButton.addTo(map);
-// Function to hide the legend when clicking on the map
-function hideLegend(e) {
+
+// Hide the legend when the mouse moves over the map
+map.on('mousemove', function () {
   var legendDiv = document.querySelector(".info.legend");
-  if (legendVisible && legendDiv && !legendDiv.contains(e.target)) {
-    legendDiv.style.height = "0"; // Minimize the legend
+  if (legendDiv.style.display === "block") {
+    legendDiv.style.display = "none";
     legendVisible = false;
   }
-}
-
-// Add event listener to the document to hide the legend when clicking outside of it and the collapse button
-map.on('click', hideLegend);
-
-
-
-
+});
 
 // North Image and scale
-
-// Scale
+map.options.scale = true;
 L.control.scale().addTo(map);
 
-// Create a custom control for the north arrow
 var northArrowControl = L.Control.extend({
   options: {
     position: "bottomleft",
   },
   onAdd: function (map) {
     var container = L.DomUtil.create("div", "leaflet-bar leaflet-control");
-    container.innerHTML =
-      '<img src="png/002-cardinal-point.png" class="border-0;" alt="" style="width: 30px; height:50px; background-color: white; border:2px solid darkblue;">';
+    container.innerHTML = '<img src="png/002-cardinal-point.png" class="border-0;" alt="" style="width: 30px; height:50px; background-color: white; border:2px solid darkblue;">';
     return container;
   },
 });
